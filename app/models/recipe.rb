aa391dc61181a87de447bf680f2818581e867837
Martyna Jasiak <martyna.jasiak@gmail.com>
2017-10-27 20:39:28 +0200

    added images

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ActiveRecord::Base
-	has_attached_file :image, styles: { medium: "400x400#"}, default_url: "/images/:style/missing.png"
+	has_attached_file :image, styles: { medium: "200x200#"}, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
